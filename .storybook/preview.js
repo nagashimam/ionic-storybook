3b465bfa78c5dc4fe64adda27e2986335669e878
nagashimam <m.nagashima@ibank.co.jp>
2020-12-19 19:28:34 +0900

    画面サイズの設定を追加

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,11 @@
-
 import { setCompodocJson } from "@storybook/addon-docs/angular";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+};
